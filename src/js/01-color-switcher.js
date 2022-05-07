d214c7a959321db481748a73bc8129fa57713897
AlenkaNahorna <agyloma@gmail.com>
2022-05-07 21:21:36 +0200

    finished first task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+let timerId = null;
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+});
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
