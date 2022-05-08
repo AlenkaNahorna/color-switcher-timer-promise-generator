0eea2fb5ca150f38dffdd292164b2a00897a13c3
AlenkaNahorna <agyloma@gmail.com>
2022-05-08 22:20:02 +0200

    start 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,16 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
+let amount = 0;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
 }
